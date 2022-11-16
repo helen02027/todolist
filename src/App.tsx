825f09d9d78e99b2ab4166fb203fb4f7971e9084
Elena Kilyanova <helen02027@gmail.com>
2022-11-16 10:53:47 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Todolist from "./components/Todolist/Todolist";
+
+function App() {
+    return (
+        <div className="App">
+         <Todolist />
+         <Todolist />
+         <Todolist />
+        </div>
+    );
+}
+
+export default App;
